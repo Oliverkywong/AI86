c47c1a9e8e9f63757673e0b1f0ee7b191c534cc6
Oliverkywong <tom23400@gmail.com>
2022-08-05 14:12:39 +0800

    login & resigter

---
+++ project(ai86).session.sql
@@ -0,0 +1,6 @@
+SELECT * FROM users where email='abc@gmail.com'
+
+SELECT player_id FROM users ORDER BY player_id DESC LIMIT 1
+SELECT player_id FROM users ORDER BY player_id LIMIT 1
+SELECT player_id FROM users ORDER BY player_id DESC LIMIT 1
+SELECT player_id FROM users ORDER BY player_id DESC
