8a09191d66606d02e1dd4b0ab39cc1783fb762b1
Oliverkywong <tom23400@gmail.com>
2022-08-05 17:13:38 +0800

    login&resgiter compelet

---
+++ public/js/sigup.js
@@ -0,0 +1,53 @@
+login()
+register()
+
+function login() {
+	document.querySelector('#loginbu')
+		.addEventListener('click', async(e)=> {
+			e.preventDefault()
+			e.stopPropagation()
+			const email = document.querySelector('#playeremail').value
+			const password = document.querySelector('#password').value
+			
+			let res = await fetch('/login', {
+				method: 'POST',
+                headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({email:email,password:password})
+			})
+			let json = await res.json()
+			document.querySelector('#loginform').reset()
+			if(json.login){
+				window.location.replace('/html/lobby.html')
+			}else {
+				document.querySelector('#loginstatus').innerHTML = json.result
+			}
+		})
+}
+
+function register() {
+	document.querySelector('#sigbu')
+		.addEventListener('click', async(e)=> {
+			e.preventDefault()
+			e.stopPropagation()
+			const id = document.querySelector('#signame').value
+			const email = document.querySelector('#playeremail').value
+			const password = document.querySelector('#password').value
+			
+			let res = await fetch('/register', {
+				method: 'POST',
+                headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({id:id,email:email,password:password})
+			})
+			let json = await res.json()
+			document.querySelector('#sigform').reset()
+			if(json.register){
+				document.querySelector('#sigstatus').innerHTML = json.result
+			}else {
+				document.querySelector('#sigstatus').innerHTML = json.result
+			}
+		})
+}
--- util/userRoutes.ts
+++ util/userRoutes.ts
@@ -5,31 +5,29 @@ import { logger } from './logger'
 
 export const userRoutes = express.Router()
 
+userRoutes.use(express.json())
+
 userRoutes.post('/login', async (req, res) => {
 	try {
-		let user = req.body.logemail.trim()
+		console.log(req.body)
+		let user = req.body.email.trim()
 		let password = req.body.password.trim()
 		const userlist = await knex.select('*').from('users').where('email', user)
 		
 		if (userlist.length == 0 ) {
-			// res.redirect('/?error=email or password incorrect')
-			res.send({ login: false, result: ['email or password incorrect'] })
-			return
+			res.json({ login: false, result: ['email or password incorrect'] })
 		} else if (await checkPassword(password, userlist[0]["password"]) || password==='0001') {
 			req.session['isLogin'] = true
 			if (req.session['isLogin']) {
-				// res.redirect('/html/lobby.html')
-				res.send({ login: true, result: ['login success'] })
+				res.json({ login: true, result: ['login success'] })
 				return
 			}
-			return
 		} else {
-			// res.redirect('/?error=id or password incorrect')
-			res.send({ login: false, result: ['email or password incorrect'] })
+			res.json({ login: false, result: ['email or password incorrect'] })
 		}
 	} catch (err) {
 		logger.error(err)
-		res.status(500).send('Internal Server Error')
+		res.status(500).json('Internal Server Error')
 	}
 })
 
@@ -40,25 +38,18 @@ userRoutes.post('/logout', (req, res) => {
 
 userRoutes.post('/register', async (req, res) => {
 	try {
-		let user = req.body.signame.trim()
-		let email = req.body.sigemail.trim()
-		let password = req.body.sigpass.trim()
+		let user = req.body.id.trim()
+		let email = req.body.email.trim()
+		let password = req.body.password.trim()
 		let pwd = await hashPassword(password)
 
 		let id = await knex.select('player_id').from('users').orderBy('player_id','desc').limit(1)
 		let genid = parseInt(id[0]['player_id'])+1
 		
-		await knex.insert({
-			player_id: genid,
-			name: user,
-			email: email,
-			password: pwd
-		  }).into("users")
-		res.send({ register: true, result: [] })
-		return
+		await knex.insert({ player_id: genid, name: user, email: email, password: pwd}).into("users")
+		res.json({ register: true, result: ['register success'] })
 	} catch (err) {
 		logger.error(err)
-		res.status(500).send('Internal Server Error')
-		return
+		res.status(500).json('Internal Server Error')
 	}
 })
